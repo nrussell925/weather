ac55215df89bd43d79278c7ebb6637809a44b755
nrussell925 <nrussell925@gmail.com>
2023-11-30 12:26:42 -0700

    Working on adding cities

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUpView
+from .views import UpdateCityView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("update_city/", UpdateCityView.as_view(), name="update_city"),
 ]
