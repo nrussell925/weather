c2e9682fdd6a237f319f7996fee4234559bca67a
nrussell925 <nrussell925@gmail.com>
2023-12-05 10:59:24 -0700

    Added Update City for All Cities

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from .views import SignUpView
-from .views import UpdateCityView
+from .views import (
+    UpdateCityView,
+    UpdateCity2View,
+    UpdateCity3View,
+    UpdateCity4View,
+    UpdateCity5View,
+)
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("update_city/", UpdateCityView.as_view(), name="update_city"),
+    path("update_city2/", UpdateCity2View.as_view(), name="update_city2"),
+    path("update_city3/", UpdateCity3View.as_view(), name="update_city3"),
+    path("update_city4/", UpdateCity4View.as_view(), name="update_city4"),
+    path("update_city5/", UpdateCity5View.as_view(), name="update_city5"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -26,3 +26,71 @@ class UpdateCityView(View):
             request.user.save()
             return redirect("home")
         return render(request, "update_city.html", {"form": form})
+
+
+class UpdateCity2View(View):
+    template_name = "update_city2.html"
+
+    def get(self, request):
+        form = SearchForm()
+        return render(request, "update_city2.html", {"form": form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data["query"]
+            request.user.city2 = city
+            request.user.save()
+            return redirect("home")
+        return render(request, "update_city2.html", {"form": form})
+
+
+class UpdateCity3View(View):
+    template_name = "update_city3.html"
+
+    def get(self, request):
+        form = SearchForm()
+        return render(request, "update_city3.html", {"form": form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data["query"]
+            request.user.city3 = city
+            request.user.save()
+            return redirect("home")
+        return render(request, "update_city3.html", {"form": form})
+
+
+class UpdateCity4View(View):
+    template_name = "update_city4.html"
+
+    def get(self, request):
+        form = SearchForm()
+        return render(request, "update_city4.html", {"form": form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data["query"]
+            request.user.city2 = city
+            request.user.save()
+            return redirect("home")
+        return render(request, "update_city4.html", {"form": form})
+
+
+class UpdateCity5View(View):
+    template_name = "update_city5.html"
+
+    def get(self, request):
+        form = SearchForm()
+        return render(request, "update_city5.html", {"form": form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data["query"]
+            request.user.city5 = city
+            request.user.save()
+            return redirect("home")
+        return render(request, "update_city5.html", {"form": form})
